b6f40bb79ac03c053a626aa73584893fff96fe54
alextavella <eu.alextavella@gmail.com>
2020-12-01 18:51:57 -0300

    Updates

---
+++ precache-manifest.4d97705dd83681e2d133568eb7c1577f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a952cd81570d2cf9879e5d26dbc0d1",
+    "url": "/alextavella/challenge-transactions/index.html"
+  },
+  {
+    "revision": "f8cb5f08734a5860bd0b",
+    "url": "/alextavella/challenge-transactions/static/js/2.afd76027.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/alextavella/challenge-transactions/static/js/2.afd76027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c37c7caf6d4fee1ae23",
+    "url": "/alextavella/challenge-transactions/static/js/main.26b0274d.chunk.js"
+  },
+  {
+    "revision": "b28c83e0b9f1f62cd4c4",
+    "url": "/alextavella/challenge-transactions/static/js/runtime-main.e9cd9cfb.js"
+  }
+]);
